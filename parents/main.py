89923af419a6e8ed7fb867c7bfa86515f6b68ee1
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2024-08-29 00:49:50 +0900

    Add server function
    It used to work, but It doesn't work now. I don't know what is going
    wrong.

--- parents/main.py
+++ parents/main.py
@@ -1,6 +1,8 @@
 import network
 import time
-import urequests as requests
+import socket
+from machine import Pin
+import rp2
 
 # https://micropython-docs-ja.readthedocs.io/ja/latest/library/network.WLAN.html
 # https://makeblock-micropython-api.readthedocs.io/en/latest/public_library/Third-party-libraries/urequests.html
@@ -9,46 +11,71 @@ import urequests as requests
 
 SSID: str = ""
 PWD: str = ""
-CH: int = 1
-ADDRS: list[str] = []  # e.g. ["192.168.1.1", "192.168.1.2"]; あらかじめ子機で固定しておく。
+CH: int = 6
+g_state_log: dict[str, dict[int, int]] = {
+    "0": {1, time.time()},
+    "1": {1, time.time()},
+    "2": {1, time.time()},
+    "3": {1, time.time()},
+    "4": {1, time.time()},
+    "5": {1, time.time()},
+}  # "ID": {state, lastchanged time}
 
 
 def start_ap():
+    rp2.country("JP")
+    print("startAP")
     ap = network.WLAN(network.AP_IF)
     ap.config(essid=SSID, password=PWD, channel=CH)
+    ap.ifconfig(('192.168.4.1', '255.255.255.0', '192.168.4.1', '192.168.4.1'))
     ap.active(True)
-    timeout: int = 10
 
-    for i in range(timeout):
+    while True:
+        print(ap.ifconfig())
+        print(ap.status())
         if ap.active():
-            return 0
+            return ap
         time.sleep(1)
-    return -1
 
 
-def get_sensor_data(ipaddr: str) -> str:
-    # TODO 各子機にリクエスト送って、レスポンスを返却
-    return ""
+def open_socket(addr):
+    con = socket.socket()
+    con.bind(addr)
+    con.listen(1)
+    return con
 
-def generate_html():
-    html = '''
-    aiu{}
-    '''.format(utime.localtime())
 
-    return html
+def handle_request(con):
+    client = con.accept()[0]
+    request = client.recv(256).decode()
+
+    # 文字数から無理やりパラメータ取得
+    id = request[-9]
+    state = request[-1]
+
+    if change_state(id, state) == 0:
+        client.send("HTTP/1.1 200 OK\n\nGood Job!")
+    else:
+        client.send("HTTP/1.1 500 Internal Server Error\n\nI'm so sorry...")
+
+    client.close()
+
+
+def change_state(id: str, state: str):
+    g_state_log[id] = [int(state), time.time()]
+    print(g_state_log)
+    return 0
 
 # TODO 表示ロジック
 # TODO 記録ロジック
 
 
 def main():
-    if start_ap() != 0:
-        return 0
+    ap = start_ap()
+    con = open_socket((ap.ifconfig()[0], 80))
 
     while True:
-        time.sleep(1)
-        for i in range(len(ADDRS)):
-            get_sensor_data(ADDRS[i])
+        handle_request(con)
 
 
 if __name__ == "__main__":
