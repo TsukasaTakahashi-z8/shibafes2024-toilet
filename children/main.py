e467ea3851810a6a04af6d6d508be2259691644d
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2024-08-28 12:22:24 +0900

    Add core0

--- children/main.py
+++ children/main.py
@@ -1,11 +1,18 @@
+import network
 import time
 from machine import Pin
+import _thread
+
+TOILET_ID = 0  # 各個室のID(1~)
 
 
 class PicoStatus:
     RUNNING = 0
     WIFI_CONNECTING = 1
     WIFI_CONECT_FAIL = 2
+
+
+g_restroom_status: int = 1
 
 
 def indicate_statusled(status):
@@ -38,13 +45,43 @@ def indicate_statusled(status):
         led.off()
 
 
-def get_restroom_status() -> bool:
-    sw: bool = Pin(0, Pin.IN, Pin.PULL_UP)  # ==1: vacant, ==0: occupied
+def get_restroom_status() -> int:
+    sw: int = Pin(0, Pin.IN, Pin.PULL_UP)  # ==1: vacant, ==0: occupied
     return sw
 
 
+def connect_network():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    while True:
+        if wlan.isconnected():
+            return wlan.ifconfig()
+
+
+def send_status(status):
+    pass  # TODO
+
+
+def core0():
+    global g_restroom_status
+    while True:
+        new_restroom_status = get_restroom_status()
+
+        if new_restroom_status != g_restroom_status:
+            with _thread.allocate_lock:
+                new_restroom_status = g_restroom_status
+
+            send_status(new_restroom_status)
+
+
+def core1():
+    while True:
+        pass  # TODO
+
+
 def main():
-    return 0
+    _thread.start_new_thread(core1, ())
+    core0()
 
 
 if __name__ == "__main__":
