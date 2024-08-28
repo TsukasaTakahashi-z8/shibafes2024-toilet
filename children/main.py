085422b26cd30c784f6bd0370b1a7b0483fe7b8e
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2024-08-28 12:42:47 +0900

    Add Wi-Fi接続処理^&^&ステータスLED表示処理

--- children/main.py
+++ children/main.py
@@ -4,6 +4,9 @@ from machine import Pin
 import _thread
 
 TOILET_ID = 0  # 各個室のID(1~)
+SSID = ""
+PWD = ""
+MY_IP = "192.168.1.x"  # xにトイレのID
 
 
 class PicoStatus:
@@ -54,8 +57,14 @@ def connect_network():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     while True:
+        wlan.connect(SSID, PWD)
+        for _ in range(30):  # 3sec
+            indicate_statusled(PicoStatus.WIFI_CONNECTING)
+
         if wlan.isconnected():
-            return wlan.ifconfig()
+            wlan_status = wlan.ifconfig
+            wlan.ifconfig((MY_IP, wlan_status[1], wlan_status[2], wlan_status[3]))
+            return wlan.status()
 
 
 def send_status(status):
@@ -76,12 +85,16 @@ def core0():
 
 def core1():
     while True:
-        pass  # TODO
+        indicate_statusled(PicoStatus.RUNNING)
 
 
 def main():
-    _thread.start_new_thread(core1, ())
-    core0()
+    if connect_network() == network.STAT_GOT_IP:
+        _thread.start_new_thread(core1, ())
+        core0()
+    else:
+        while True:
+            indicate_statusled(PicoStatus.WIFI_CONECT_FAIL)
 
 
 if __name__ == "__main__":
