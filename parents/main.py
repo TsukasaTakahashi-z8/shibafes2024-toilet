0184ba4bdfc98fb2ba412f816a68d72a08fb6882
Rcuhi <rappinngu2006@gmail.com>
2024-08-29 11:30:53 +0900

    maybe fixed it

--- parents/main.py
+++ parents/main.py
@@ -21,21 +21,49 @@ g_state_log: dict[str, dict[int, int]] = {
     "5": {1, time.time()},
 }  # "ID": {state, lastchanged time}
 
+def status2message(status):
+    # 1000
+    if status == network.STAT_IDLE: return "STAT_IDLE"
+    # 1001
+    if status == network.STAT_CONNECTING: return "STAT_CONNECTING"
+    # 202
+    if status == network.STAT_WRONG_PASSWORD: return "STAT_WRONG_PASSWORD"
+    # 201
+    if status == network.STAT_NO_AP_FOUND: return "STAT_NO_AP_FOUND"
+    # 1010
+    if status == network.STAT_GOT_IP: return "STAT_GOT_IP"
+    # 203
+    if status == network.STAT_ASSOC_FAIL: return "STAT_ASSOC_FAIL"
+    # 200
+    if status == network.STAT_BEACON_TIMEOUT: return "STAT_BEACON_TIMEOUT"
+    # 204
+    if status == network.STAT_HANDSHAKE_TIMEOUT: return "STAT_HANDSHAKE_TIMEOUT"
+    # :(
+    return "UNKNOWN_STATUS"
 
 def start_ap():
     rp2.country("JP")
     print("startAP")
     ap = network.WLAN(network.AP_IF)
-    ap.config(essid=SSID, password=PWD, channel=CH)
+    ap.config(ssid=SSID, key=PWD, channel=CH)
     ap.ifconfig(('192.168.4.1', '255.255.255.0', '192.168.4.1', '192.168.4.1'))
     ap.active(True)
 
     while True:
         print(ap.ifconfig())
         print(ap.status())
+        status = ap.status()
         if ap.active():
             return ap
-        time.sleep(1)
+        if status == network.STAT_IDLE:
+            time.sleep(1)
+            continue
+        if status == network.STAT_CONNECTING:
+            time.sleep(1)
+            continue
+
+        print(f"something went wrong. {status2message(status)}")
+        break
 
 
 def open_socket(addr):
