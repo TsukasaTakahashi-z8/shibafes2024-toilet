4909da0f1b6f927e4ead6524344c5a179ecb7834
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2024-09-26 00:48:37 +0900

    Add statusLED & displayLED function

--- parents/main.py
+++ parents/main.py
@@ -17,22 +17,104 @@ g_state_log: dict[str, dict[int, int]] = {
     "5": {"state": 1, "time": time.time()},
     "6": {"state": 1, "time": time.time()},
 }  # "ID": {state, lastchanged time}
+g_LED = {
+    "1": [Pin(17, Pin.OUT), Pin(16, Pin.OUT)],
+    "2": [Pin(13, Pin.OUT), Pin(12, Pin.OUT)],
+    "3": [Pin(9, Pin.OUT), Pin(8, Pin.OUT)],
+    "4": [Pin(5, Pin.OUT), Pin(4, Pin.OUT)],
+    "5": [Pin(1, Pin.OUT), Pin(0, Pin.OUT)],
+    "6": [Pin(28, Pin.OUT), Pin(27, Pin.OUT)],
+    "7": [Pin(26, Pin.OUT), Pin(22, Pin.OUT)],
+    "8": [Pin(21, Pin.OUT), Pin(20, Pin.OUT)]
+}
 
 
-def start_ap():
-    rp2.country("JP")
-    print("startAP")
-    ap = network.WLAN(network.AP_IF)
-    ap.config(essid=SSID, password=PWD, channel=CH)
-    ap.ifconfig(('192.168.4.1', '255.255.255.0', '192.168.4.1', '192.168.4.1'))
-    ap.active(True)
+class PicoStatus:
+    RUNNING = 0
+    WIFI_CONNECTING = 1
+    WIFI_CONECT_FAIL = 2
 
-    while True:
-        print(ap.ifconfig())
-        print(ap.status())
-        if ap.active():
-            return ap
+
+def indicate_statusled(status):
+    led = Pin("LED", Pin.OUT)
+
+    # may be not support match-case statement
+    if status == PicoStatus.RUNNING:
+        led.on()
         time.sleep(1)
+        led.off()
+        time.sleep(1)
+
+    elif status == PicoStatus.WIFI_CONNECTING:
+        led.on()
+        time.sleep(0.1)
+        led.off()
+        time.sleep(0.1)
+
+    elif status == PicoStatus.WIFI_CONECT_FAIL:
+        led.on()
+        time.sleep(0.1)
+        led.off()
+        time.sleep(0.1)
+        led.on()
+        time.sleep(0.1)
+        led.off()
+        time.sleep(0.5)
+
+    else:
+        led.off()
+
+
+def check_display_led():
+    for i in range(1, 8+1):
+        g_LED[str(i)][0].on()
+        g_LED[str(i)][1].on()
+
+    time.sleep(2)
+
+    for i in range(1, 8+1):
+        g_LED[str(i)][0].off()
+        g_LED[str(i)][1].on()
+
+    time.sleep(1)
+
+    for i in range(1, 8+1):
+
+        g_LED[str(i)][0].on()
+        g_LED[str(i)][1].off()
+
+    time.sleep(1)
+
+    for i in range(1, 8+1):
+        g_LED[str(i)][0].off()
+        g_LED[str(i)][1].off()
+
+    time.sleep(1)
+
+    for i in range(1, 8+1):
+        g_LED[str(i)][0].on()
+        g_LED[str(i)][1].off()
+        time.sleep(0.1)
+        g_LED[str(i)][0].off()
+        g_LED[str(i)][1].on()
+        time.sleep(0.1)
+        g_LED[str(i)][0].off()
+        g_LED[str(i)][1].off()
+        time.sleep(0.1)
+
+
+def connect_network():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    # TODO add wlan.conf() to set my IPaddr(192.168.4.1)
+    while True:
+        wlan.connect(SSID, PWD)
+        wlan.ifconfig(('192.168.4.1', '255.255.255.0', '192.168.4.200', '192.168.4.200'))
+        for _ in range(30):  # 3sec
+            indicate_statusled(PicoStatus.WIFI_CONNECTING)
+
+            if wlan.isconnected():
+                return wlan
 
 
 def open_socket(addr):
@@ -69,8 +151,9 @@ def change_state(id: str, state: str):
 
 
 def main():
-    ap = start_ap()
-    con = open_socket((ap.ifconfig()[0], 80))
+    check_display_led()
+    wlan = connect_network()
+    con = open_socket((wlan.ifconfig()[0], 80))
 
     while True:
         handle_request(con)
